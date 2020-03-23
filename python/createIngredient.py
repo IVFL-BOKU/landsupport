8253371ba48a9f6fc5a05a11048226202fa2a6ee
Mateusz Żółtak <mateusz.zoltak@boku.ac.at>
2020-03-23 11:50:11 +0100

    Rasdaman-helper python script adjusted for scripts/rasdaman.R

---
+++ python/createIngredient.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser(description='Generates Rasdaman ingridient files for directories containing single coverage(band) data in the "{YYYY-MM-DD}_{whatever}" file naming schema')
+parser.add_argument('--serviceUrl', default='http://localhost:9009/rasdaman/ows')
+parser.add_argument('--tmpDir', default='/tmp')
+parser.add_argument('--crsResolver', default='http://www.opengis.net/def/')
+parser.add_argument('--mock', action="store_true")
+parser.add_argument('--noWms', action="store_true")
+parser.add_argument('--targetFile', help='location of a ingredient file to be created (by default "{dataDir}.json")')
+parser.add_argument('dataDir')
+parser.add_argument('coverageName')
+args = parser.parse_args()
+
+tmpl = """{{
+  "config": {{
+    "service_url": "{serviceUrl}",
+    "tmp_directory": "{tmpDir}",
+    "crs_resolver": "{crsResolver}",
+    "default_crs": "http://www.opengis.net/def/crs/EPSG/0/3035",
+    "mock": {mock},
+    "automated": true
+  }},
+  "input": {{
+    "coverage_id": "{name}",
+    "paths": [
+      "{dataDir}/*"
+    ]
+  }},
+  "recipe": {{
+    "name": "time_series_irregular",
+    "options": {{
+      "time_parameter": {{
+        "filename": {{
+          "regex": "^.*/([-0-9]+)_[^/]*$",
+          "group": 1
+        }},
+        "datetime_format": "YYYY-MM-DD"
+      }},
+      "time_crs": "http://www.opengis.net/def/crs/OGC/0/AnsiDate",
+      "wms_import": {wmsImport}
+    }}
+  }}
+}}"""
+
+targetFile = args.targetFile
+if targetFile is None:
+    targetFile = args.dataDir + '.json'
+
+with open(targetFile, 'w') as f:
+    f.write(tmpl.format(
+        serviceUrl=args.serviceUrl, tmpDir=args.tmpDir, dataDir=args.dataDir, 
+        crsResolver=args.crsResolver, 
+        mock='true' if args.mock else 'false', 
+        name=args.coverageName, 
+        wmsImport='false' if args.noWms else 'true'
+    ))
+
--- python/rename2rasdaman.py
+++ python/rename2rasdaman.py
@@ -1,34 +1,47 @@
 #!/usr/bin/python3
 
 import argparse
-import gdal
 import os
 import re
 
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='Creates a directory with rasdaman import-ready files structure by creating symlinks to the cubeR native file structure.')
 parser.add_argument('--dataDir', default='/eodc/private/boku/ACube2/tiles', help='directory containing tiles subdirs')
-parser.add_argument('--targetDir', default='/eodc/private/boku/ACube2/upload', help='base directory for band name-based rasdaman collection subdirs')
 parser.add_argument('--dateFrom', default='1900-01-01')
 parser.add_argument('--dateTo', default='3000-01-01')
-parser.add_argument('match', help='string to be matched against raster file names (typically a band name, sometimes combined with a period indication, e.g. "NDVI2q98" or "m1_DOYMAXNDVI2")')
-parser.add_argument('--tiles', nargs='*', default=[])
+parser.add_argument('--tilesFile', help='path to a file storing a list of tiles to be processed (one per line)')
+parser.add_argument('band', help='name of a band to be processed')
+parser.add_argument('targetDir', help='directory to write symbolic links to')
 args = parser.parse_args()
 
+bandF = '_%s_' % args.band
+
+tiles = []
+if args.tilesFile is not None:
+    with open(args.tilesFile) as f:
+        tiles = f.readlines()
+    tiles = [x.strip() for x in tiles]
+
+if not os.path.exists(args.targetDir):
+    os.makedirs(args.targetDir, 0o770)
+
 ext = None
+try: import gdal
+except: ext = 'tif'
 
 utms = os.listdir(args.dataDir)
 utms.sort()
+targetPaths = []
 for utm in utms:
     if not os.path.isdir(os.path.join(args.dataDir, utm)):
         continue
     files = os.listdir(os.path.join(args.dataDir, utm))
     files.sort()
     for fl in files:
-        if re.search(args.match, fl):
+        if bandF in fl:
             localPath = os.path.join(args.dataDir, utm, fl)
             (date, band, tile) = fl[0:-4].split('_')
-            if len(args.tiles) > 0 and tile not in args.tiles:
+            if len(tiles) > 0 and tile not in tiles:
                 continue
 
             period = re.sub('^([0-9]+(-[0-9]+)?(-[0-9]+)?)', '', date)
@@ -36,7 +49,7 @@ for utm in utms:
                 period = 'none'
 
             date = re.sub('^([0-9]+(-[0-9]+)?(-[0-9]+)?).*$', '\\1', date)
-            if date > args.dateTo or date < args.dateFrom:
+            if date > args.dateTo[0:len(date)] or date < args.dateFrom[0:len(date)]:
                 continue
             if len(date) == 4:
                 date += '-01'
@@ -49,12 +62,15 @@ for utm in utms:
                 if 'JP2' in gdal.Open(localPath).GetDriver().ShortName:
                     ext = '.jp2'
 
-            collectionDir = os.path.join(args.targetDir, band)
-            targetPath = os.path.join(collectionDir, '_'.join((date, period, band, tile)) + ext)
+            targetPath = os.path.join(args.targetDir, '_'.join((date, period, band, tile)) + ext)
 
-            if not os.path.exists(collectionDir):
-                os.makedirs(collectionDir)
-            if os.path.islink(targetPath):
+            if os.path.islink(targetPath) or os.path.isfile(targetPath):
                 os.unlink(targetPath)
             os.symlink(localPath, targetPath)
+            targetPaths.append(os.path.basename(targetPath))
+toDel = set(os.listdir(args.targetDir)) - set(targetPaths)
+for i in toDel:
+    try:
+        os.unlink(os.path.join(args.targetDir, i))
+    except: pass
 
